410d1ab969decfce22f6c8b1fc595e79fb736b2f
aliabbdallah <149956411+aliabbdallah@users.noreply.github.com>
2025-04-18 20:43:07 +0200

    Delete .anaconda directory

--- .anaconda/navigator/a.tool
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/osascript
-tell application "Terminal"
-    activate
-    do script ". /Users/AliA/anaconda3/bin/activate && conda activate /Users/AliA/anaconda3/envs/bigData; "
-end tell
--- .anaconda/navigator/scripts/notebook.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-. /Users/AliA/anaconda3/bin/activate /Users/AliA/anaconda3
-open /Users/AliA/anaconda3/bin/jupyter_mac.command >/Users/AliA/.anaconda/navigator/scripts/notebook-out-1.txt 2>/Users/AliA/.anaconda/navigator/scripts/notebook-err-1.txt
